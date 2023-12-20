6c5fbef8dd2ade856382c72922de4a858954ab95
guilherme rabelo <guilherme.rabelo@matriz.fornecedoramaquinas.com.br>
2023-12-20 15:25:36 -0300

    Feat/Criando funcao de envio de emails

--- src/email.py
+++ src/email.py
@@ -1,9 +1,7 @@
 import win32com.client as win32
+from jinja2 import Environment, FileSystemLoader
 import os
 import json
-
-# html_file_path_fatura = os.path.join(os.path.dirname(__file__), 'assets', 'emailFatura.html')
-# html_file_path_lancamento = os.path.join(os.path.dirname(__file__), 'assets', 'emailLancamento.html')
 
 def envioDoEmail(tipo, dados):
     with open("../config/config.json", "r", encoding="utf-8") as file:
@@ -13,26 +11,62 @@ def envioDoEmail(tipo, dados):
     outlook = win32.Dispatch('outlook.application')
     email = outlook.CreateItem(0)
     if tipo == 'fatura':
-        # with open(html_file_path_fatura, 'r', encoding='utf-8') as file:
-        #     html_body = file.read()
-        #     html_body = 'Body fatura'
-        html_body = 'Body fatura'
-            # html_body = html_body.format(cliente=dados['cliente'], emissao=dados['emissao'], empresa=dados['empresa'], numeroNotaFiscal=dados['numeroNota'], vencimentoNotaFiscal=dados['vencimento'], valorBoleto=dados['boleto'], valorTitulo=dados['nota'])
+        env = Environment(loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), '../assets')))
+        template = env.get_template('emailFatura.html')
+        html_body = template.render(data=dados['parcelas'])
+        html_body = html_body.format(dataEmail=dados['dataEmail'], empresa=dados['empresa'], fornecedor=dados['fornecedor'], totalBruto=dados['totalBruto'], totalJuros=dados['totalJuros'], deducoesImpostos=dados['deducoesImpostos'], descontos=dados['descontos'], totalLiquido=dados['totalLiquido'])
 
     else:
-        # with open(html_file_path_lancamento, 'r', encoding='utf-8') as file:
-        #     html_body = file.read()
-        #     html_body = 'Body lancamento'
-        html_body = 'Body lancamento'
-            # html_body = html_body.format(cliente=dados['cliente'], emissao=dados['emissao'], empresa=dados['empresa'], numeroNotaFiscal=dados['numeroNota'], vencimentoNotaFiscal=dados['vencimento'], valorBoleto=dados['boleto'], valorTitulo=dados['nota'])
+        print('oi')
+        html_file_path_lancamento = os.path.join(os.path.dirname(__file__), '../assets', 'emailLancamento.html')
+        with open(html_file_path_lancamento, 'r', encoding='utf-8') as file:
+            html_body = file.read()
+            html_body = html_body.format(referencia=dados['referencia'], dataPagamento=dados['dataPagamento'], valorTitulo=dados['valorTitulo'], dataEmail=dados['dataEmail'], empresa=dados['empresa'], fornecedor=dados['fornecedor'], totalBruto=dados['totalBruto'], totalJuros=dados['totalJuros'], deducoesImpostos=dados['deducoesImpostos'], descontos=dados['descontos'], totalLiquido=dados['totalLiquido'])
 
-    # email.To = dados['email']
-    email.To = 'guilherme.rabelo@grupofornecedora.com.br'
+    email.To = dados['para']
     email.BCC = emailsCopiaOculta
     email.Subject = 'Comprovante de pagamento'
     email.HTMLBody = html_body
 
     email.Send()
 
-envioDoEmail('fatura', '')
-envioDoEmail('lancamento', '')
+# envioDoEmail('fatura', {
+#     'para': 'guilherme.rabelo@grupofornecedora.com.br',
+#     'dataEmail': '20/12/2023',
+#     'empresa': 'FME MATRIZ 0001/69',
+#     'fornecedor': 'FORNECEDORA MAQUINAS E EQUIPAMENTOS LTDA',
+#     'totalBruto': '1.000,00',
+#     'totalJuros': '54,00',
+#     'deducoesImpostos': '1212,54',
+#     'descontos': '121,35',
+#     'totalLiquido': '875,28',
+#     'parcelas': [
+#         {
+#             'referencia': '122',
+#             'parcela': '01',
+#             'dataPagamento': '15/11/2023',
+#             'valorTitulo': '124,00'
+#         },
+#         {
+#             'referencia': '122',
+#             'parcela': '01',
+#             'dataPagamento': '15/11/2023',
+#             'valorTitulo': '124,00'
+#         },
+#     ]
+# })
+
+# envioDoEmail('lancamento', {
+#     'para': 'guilherme.rabelo@grupofornecedora.com.br',
+#     'dataEmail': '20/12/2023',
+#     'empresa': 'FME MATRIZ 0001/69',
+#     'fornecedor': 'FORNECEDORA MAQUINAS E EQUIPAMENTOS LTDA',
+#     'totalBruto': '1.000,00',
+#     'totalJuros': '54,00',
+#     'deducoesImpostos': '1212,54',
+#     'descontos': '121,35',
+#     'totalLiquido': '875,28',
+#     'referencia': '122',
+#     'dataPagamento': '15/11/2023',
+#     'valorTitulo': '124,00'
+# })
